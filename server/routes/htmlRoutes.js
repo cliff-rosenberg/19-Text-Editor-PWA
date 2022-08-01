b66c11a5199eb36fdde02cc057b0fb5b3a4e8a23
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-01 13:07:20 -0700

    added code comments

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,10 @@
+// set up path here
+// The Path module provides a way of working with directories and file paths
+// see https://nodejs.org/api/path.html for more information
+// used below to set up Express static paths
 const path = require('path');
 
+// export the base route, which serves up a static page for 'index.html' homepage
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
